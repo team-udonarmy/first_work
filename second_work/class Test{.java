3c0bc1e43500b7f73acd8238ad207d970f6b26e3
Masadekaiketu <masa.kikai0304@gmail.com>
2022-05-21 12:06:05 +0900

    change file

--- second_work/class Test{.java
+++ second_work/class Test{.java
@@ -1,5 +1,5 @@
-class Test{
-    public static void(String[] args){
-        System.print.out("hello")
+public class Test{
+    public static void main(String[] args){
+        System.out.println("hello")
     }
 }
